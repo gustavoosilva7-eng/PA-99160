6582801f7a2f86fea7e8575b9d90ca8835f13564
YOUR NAME <gustavo.o.silva7@ba.estudante.senai.br>
2026-02-26 21:37:44 -0300

    Sabooooouurrr JavaScript

---
+++ 10.função.js
@@ -0,0 +1,28 @@
+function somar(a,b){
+    return a + b
+}
+
+const soma = somar(2,3)
+console.log(`soma: ${soma}`)
+
+
+function subtrair(a,b){
+    return a - b
+}
+
+const subtracao = subtrair(5,2)
+console.log(`subtração: ${subtracao}`)
+
+function multiplicar(a,b){
+    return a * b
+}   
+
+const multiplicacao = multiplicar(2,3)
+console.log(`multiplicação: ${multiplicacao}`)
+
+function dividir(a,b){
+    return a / b
+}   
+
+const divisao = dividir(10,2)
+console.log(`divisão: ${divisao}`)
