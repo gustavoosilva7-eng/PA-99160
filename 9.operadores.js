13aa614a48f23e91b1c87b2c44c643339ba3c34e
YOUR NAME <gustavo.o.silva7@ba.estudante.senai.br>
2026-02-26 21:25:22 -0300

    JavaScript

---
+++ 9.operadores.js
@@ -0,0 +1,25 @@
+ let idade = 10
+ let idade2 = "10"
+
+ console.log("comparando conteudo")
+ console.log(`o valor ${idade} é igual a ${idade2}`)
+ console.log(idade == idade2) 
+
+ console.log("\n== comparando  conteudo")
+ console.log(`o valor ${idade} é igual a ${idade2}`)
+ console.log(idade != idade2) 
+
+ console.log("\n== comparando tipo e conteudo")
+ console.log(`o valor ${idade} é igual a ${idade2}`)
+ console.log(idade === idade2) 
+
+ console.log("\n== comparando  tipo e conteudo")
+ console.log(`o valor ${idade} é igual a ${idade2}`)
+ console.log(idade !== idade2) 
+ 
+ console.log("\n== AND")
+ console.log("1 < 2 && 1 < 3") 
+ 
+ console.log("\n== OR")
+ console.log("10 < 2 || 1 < 3") 
+ console.log(1 < 2 || 1 < 3) 
